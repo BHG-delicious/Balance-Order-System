a9c7be83f9e3815529afc71385fd9aebc2cc56b6
大家政便當廠 <danieltsao2@gmail.com>
2026-09-18 22:44:04 +0800

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -6,13 +6,3 @@ const ASSETS = ['index.html', 'manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-
-// 在 Service Worker 檔案中 (sw.js)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // 如果快取中有，直接回傳快取；若沒有，才向伺服器發送請求
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
