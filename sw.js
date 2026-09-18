b030a75996cfcf8836042cf603c946882f30d950
大家政便當廠 <danieltsao2@gmail.com>
2026-09-18 22:37:58 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-const CACHE_NAME = 'Balance-Order-System-v2.6.1';
-const ASSETS = ['index.html', 'manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
